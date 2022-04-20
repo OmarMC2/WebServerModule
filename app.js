fd4c3f4dd5085b7b12a107877aba143cbf3614bd
Omar <omarmc2@gmail.com>
2022-04-20 00:52:10 -0500

    sirviendo contenido estático como servidor utilizando express

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 
+
+//servir contenido estático
+
+
+app.use( express.static('public') );
+
+
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
