4ae977e4166adccd004946e00fccffaa3627529d
Omar <omarmc2@gmail.com>
2022-04-24 19:32:03 -0500

    añado servidor estático de handlebars y con configfuración de .env

--- app.js
+++ app.js
@@ -1,29 +1,43 @@
+require('dotenv').config();
 const express = require('express')
-const app = express()
-
+const hbs = require('hbs');
+const app = express();
+const port = process.env.PORT
 
 //servir contenido estático
 
+
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.use( express.static('public') );
 
 
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/public/index.html')
-})
+  res.render('home', {
+    nombre: 'Omar Mijangos',
+    titulo: 'Curso node'
+  });
+});
 
 app.get('/generic', function (req, res) {
-    res.sendFile(__dirname + '/public/generic.html')
-  })
+    res.render( 'generic', {
+      nombre: 'Omar Mijangos',
+      titulo: 'Curso de Node'
+    })
+  });
 app.get('/elements', function (req, res) {
-    res.sendFile(__dirname + '/public/elements.html')
-  })
+    res.render('elements', {
+      nombre: 'Omar Mijangos',
+      titulo: 'Curso de Node'
+    });
+  });  
 
   app.get('*', function (req, res) {
     res.send(' 404 | Page not found')
   })
 
-app.listen(8080)
 
-
+app.listen( port )
